f1faec8a371bd5638062715fa05eb74738f60b50
DarshanU25 <ubhaledarshan23@gmail.com>
2025-04-18 00:06:21 +0530

    main.py

--- main.py
+++ main.py
@@ -33,5 +33,5 @@ def get_signals():
 
 # Start Uvicorn dynamically for Railway
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
